1e28755c8f0e8f928ce8cb2f7ee1edce118fe33f
Scott Rabara <srabara@iqms.com>
2019-03-07 02:00:55 -0800

    Added new logo background for landingscene; added random scene; added hidden menu for testing

---
+++ Assets/PlayRandomGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+
+public class PlayRandomGame : MonoBehaviour
+{
+
+    public void PlayRandomScene()
+    {
+        var sceneIndex = Random.Range(1, 4);
+
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
