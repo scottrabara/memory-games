ae68a3a17dccdbf40c3f8fd6db00eeca71897fc7
Daniel Blike <dblike@iqms.com>
2019-03-07 00:13:32 -0800

    Dank commit yo...

---
+++ Assets/DialogueTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public void TriggerDialog()
+    {
+        var dialogueManager = FindObjectOfType<DialogueManager>();
+        if (dialogueManager == null)
+            Debug.Log("Dialogue Manager not initialized");
+        else
+            dialogueManager.StartDialogue(dialogue);
+    }
+}
