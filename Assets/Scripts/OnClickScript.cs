73310d399ed2b786b6f5ecb4881449b0bed1f248
igomez92 <gomez.isidro1@gmail.com>
2019-03-06 18:56:46 -0800

    Example Project
    Added Scene to showcase spawning and onclick events.

---
+++ Assets/Scripts/OnClickScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public SpriteRenderer sRender;
+    void Start()
+    {
+        sRender = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        sRender.color = Color.white;
+    }
+}
---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public Transform spawnPos;
+    public GameObject shape;
+    private Color[] colorArray = { Color.red, Color.blue, Color.green, Color.yellow };
+    public Sprite[] spriteArray = new Sprite[3];
+    public SpriteRenderer sRender;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        for (int x = 0; x<5; x++)
+        {
+            for (int y = 0; y < 5; y++)
+            {
+
+                sRender = shape.GetComponent<SpriteRenderer>();
+                sRender.color = colorArray[Random.Range(0, 4)];
+                sRender.sprite = spriteArray[Random.Range(0, 3)];
+                Instantiate(shape, new Vector3(spawnPos.position.x + x, spawnPos.position.y + y, 0), spawnPos.rotation);
+
+            }
+                
+        }
+        sRender.color = Color.white;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
