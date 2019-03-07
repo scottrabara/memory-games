6968b420a2dafc47325f2675a56fe0445a96d99e
Scott Rabara <srabara@iqms.com>
2019-03-07 10:58:49 -0800

    Add piece physics for checkers scene

--- Assets/Scripts/Models/Piece.cs
+++ Assets/Scripts/Models/Piece.cs
@@ -6,11 +6,14 @@ public class Piece : MonoBehaviour
 {
     private Vector3 screenPoint;
     private Vector3 offset;
+    private Rigidbody rigidBody;
+    private BoxCollider boxCollider;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = gameObject.AddComponent<Rigidbody>();
+        boxCollider = gameObject.AddComponent<BoxCollider>();
     }
 
     // Update is called once per frame
